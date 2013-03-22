f37e795ec5cf6c308848f09c6a1bfa60beedb850
Alex Curran <aml.curran@gmail.com>
2013-03-22 14:06:11 +0000

    Boilerplate release

---
+++ src/com/espian/ticktock/TickTockActivity.java
@@ -0,0 +1,15 @@
+package com.espian.ticktock;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class TickTockActivity extends Activity {
+	/**
+	 * Called when the activity is first created.
+	 */
+	@Override
+	public void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.main);
+	}
+}
---
+++ src/com/espian/ticktock/TickTockProvider.java
@@ -0,0 +1,44 @@
+package com.espian.ticktock;
+
+import android.content.ContentProvider;
+import android.content.ContentValues;
+import android.database.Cursor;
+import android.net.Uri;
+
+/**
+ * Author: Alex Curran
+ * Date: 22/03/2013
+ */
+public class TickTockProvider extends ContentProvider {
+
+
+	@Override
+	public boolean onCreate() {
+		return false;
+	}
+
+	@Override
+	public Cursor query(Uri uri, String[] projection, String selection, String[] selectionArgs, String sortOrder) {
+		return null;
+	}
+
+	@Override
+	public String getType(Uri uri) {
+		return null;
+	}
+
+	@Override
+	public Uri insert(Uri uri, ContentValues values) {
+		return null;
+	}
+
+	@Override
+	public int delete(Uri uri, String selection, String[] selectionArgs) {
+		return 0;
+	}
+
+	@Override
+	public int update(Uri uri, ContentValues values, String selection, String[] selectionArgs) {
+		return 0;
+	}
+}
